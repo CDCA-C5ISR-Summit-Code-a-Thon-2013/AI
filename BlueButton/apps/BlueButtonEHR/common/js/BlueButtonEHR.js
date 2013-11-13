62c4faa5d1f578e7678ce0d75ae29905969337f4
jeffet <jeffet99@hotmail.com>
2013-11-13 16:39:42 -0500

    Changes to get cardiac integrated

--- BlueButton/apps/BlueButtonEHR/common/js/BlueButtonEHR.js
+++ BlueButton/apps/BlueButtonEHR/common/js/BlueButtonEHR.js
@@ -33,5 +33,41 @@ function wlCommonInit(){
 	
 	$.mobile.page.prototype.options.domCache = true;	
 	$.mobile.loadPage("app-pages/Appointments.html", {});
+	$.mobile.loadPage("app-pages/cardiac.html", {reloadPage: true});
 
 }
+
+$( document ).on('pagebeforeshow','#cardiac', function(event){
+    extractData().then(function(p){
+        var missing_data_array = report_missing_data(p);
+        if (missing_data_array.length > 0) {
+          $('#errors')
+          .show()
+          .html(function(){
+            var out = '<h2>Can\'t Compute Cardiovascular Risk Score</h2><p>Can\'t compute cardiovascular risk score from available data because of the following issues:</p><ul>';
+              $.each(missing_data_array, function(i, v){ out += '<li>' + missing_data_array[i] + '</li>'; }) 
+              return out + '</ul>';
+          });
+
+          return; // abort script
+          } else {
+          $('#holder').show();
+          draw_visualization();
+        }			 
+
+      });
+
+      // checks for data needed to compute risk score. returns a possibly empty array of strings.
+      // if the array is empty everything's ok. TODO: seperate missing and out of range criteria
+      var report_missing_data = function(p){
+        var a = [];
+        if (p.gender.value != 'male' && p.gender.value != 'female') a.push('Gender is required')
+        if (p.sbp.value < 105 || p.sbp.value > 200) a.push('Systolic blood pressure must be between 105 and 200mm/Hg')
+        if (p.cholesterol.value < 140 || p.cholesterol.value > 400) a.push('Total cholesterol must be between 140 and 400mg/dL')
+        if (p.HDL.value < 30 || p.HDL.value > 150) a.push('HDL cholesterol must be between 30 and 150mg/dL')
+        if (p.hsCRP.value < 0.03 || p.hsCRP.value > 20) a.push('hsCRP must be between 0.03 and 20mg/L')
+        if (p.smoker_p.value === false || p.smoker_p.value === true) {} else { a.push('Current smoking status must be defined') }
+        if (p.fx_of_mi_p.value === false || p.fx_of_mi_p.value === true) {} else { a.push('Family history of heart attack before the age of 60 must be defined') }
+        return a;
+      };
+});
